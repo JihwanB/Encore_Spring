16192f9970884faf9e6318e9da2fde1573b00e2f
JihwanB <leyqorwlghks@gmail.com>
2024-03-05 17:47:31 +0900

    Added methods: updateByIdx(), count(), deleteByIdx(), and findAll()

--- src/main/java/com/example/encore_spring_pjt/mapper/BoardMapper.java
+++ src/main/java/com/example/encore_spring_pjt/mapper/BoardMapper.java
@@ -4,6 +4,8 @@ import com.example.encore_spring_pjt.domain.BoardRequest;
 import com.example.encore_spring_pjt.domain.BoardResponse;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /*
  encore_board_tbl 과 CRUD 작업을 위한 추상메서드 선언
  Mapper 는 기존의 DAO 와 동일한 작업을 진행하는 것
@@ -12,6 +14,20 @@ import org.apache.ibatis.annotations.Mapper;
 public interface BoardMapper {
     // insert
     void save(BoardRequest params);
+
     // select (single finder)
     BoardResponse findByIdx(BoardRequest params);
+
+    // update (title, content, writer)
+    void updateByIdx(BoardRequest params);
+
+    // 레코드의 건수를 count
+    int count();
+
+    // delete (idx)
+    void deleteByIdx(BoardRequest params);
+
+    // select (multi finder)
+    List<BoardResponse> findAll();
+
 }
--- src/test/java/com/example/encore_spring_pjt/MybatisORMTests.java
+++ src/test/java/com/example/encore_spring_pjt/MybatisORMTests.java
@@ -3,9 +3,14 @@ package com.example.encore_spring_pjt;
 import com.example.encore_spring_pjt.domain.BoardRequest;
 import com.example.encore_spring_pjt.domain.BoardResponse;
 import com.example.encore_spring_pjt.mapper.BoardMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class MybatisORMTests {
@@ -27,12 +32,56 @@ public class MybatisORMTests {
     }
 
     @Test
-    public void ormFind() {
+    public void ormFind() throws JsonProcessingException {
         System.out.println("debug finder >>>>>>>>>>>>>>>>>>>>>> ");
         BoardRequest request = new BoardRequest();
         request.setIdx(1);
         BoardResponse response = boardMapper.findByIdx(request);
         System.out.println("debug find result >>>>> ");
         System.out.println(response);
+        System.out.println(">>>>>>>>>>>>>>>>>>> json ");
+        String boardJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(response);
+        System.out.println(boardJson);
     }
+
+    @Test
+    public void ormUpdate() {
+        System.out.println("debug update >>>>>>>>>>>>>>> ");
+        BoardRequest request = new BoardRequest();
+        request.setIdx(1);
+        request.setTitle("제목 수정");
+        request.setContent("변경된 내용");
+        request.setWriter("jslim");
+        boardMapper.updateByIdx(request);
+        System.out.println(request);
+        System.out.println("debug >>>>>>>>> update success ");
+    }
+
+    @Test
+    public void ormCount() {
+        System.out.println("debug count >>>>>>>>>>>>>>> ");
+        System.out.println(boardMapper.count());
+        System.out.println("debug >>>>>>>>>>>> count success ");
+    }
+
+    @Test
+    public void ormDelete() {
+        System.out.println("debug delete >>>>>>>>>>>>>> ");
+        BoardRequest request = new BoardRequest();
+        request.setIdx(2);
+        boardMapper.deleteByIdx(request);
+        System.out.println(request);
+        System.out.println("debug >>>>>>>>>>>> delete success");
+    }
+
+    @Test
+    public void ormFindAll() {
+        System.out.println("debug findAll >>>>>>>>>>>>>>>>");
+        List<BoardResponse> boardResponseList = boardMapper.findAll();
+        for (BoardResponse response : boardResponseList) {
+            System.out.println(response);
+        }
+        System.out.println("debug >>>>>>>>>>>>> findAll success ");
+    }
+
 }
