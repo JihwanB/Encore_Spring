7206c138951f91fe0f9ec5bd01f6b5626218e9db
JihwanB <leyqorwlghks@gmail.com>
2024-04-09 16:26:51 +0900

    Added a directory for Vue.js study

---
+++ encore-vue-pjt/src/components/WorkForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form>
+        <div class="d-flex">
+            <div class="flex-grow-1 mr-2">
+                <input type="text" class="form-control" placeholder="new work...">
+            </div>
+
+            <div>
+                <button class="btn btn-primary">ADD</button>
+            </div>
+        </div>
+
+        <!-- v-show 사용해서, null 값이 입력되었을 때, 오류메시지 출력 -->
+        <div class="error">
+            This field cannot be empty.
+        </div>
+
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
